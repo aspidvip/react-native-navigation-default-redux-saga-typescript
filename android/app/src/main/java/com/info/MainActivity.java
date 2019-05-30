e721b2257f94df82d32053c69b27e0ff83cb0798
Andrey Bondarenko <bondarenko@mail.tvil.ru>
2019-05-30 16:00:19 +1000

    push android

---
+++ android/app/src/main/java/com/info/MainActivity.java
@@ -0,0 +1,16 @@
+package com.info;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+
+        /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+
+    protected String getMainComponentName() {
+        return "info";
+    }
+}
